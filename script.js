7e360f27234d69bbdcca48bf6ee309cced8520fd
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-03-12 18:47:47 +0100

    populate display with clicks in buttons

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ function operate(num1, operator, num2) {
   if (operator === "*") return multiply(num1, num2);
   if (operator === "/") return divide(num1, num2);
 }
+
+const digitButtons = document.querySelectorAll(".digit-btn");
+const displayNum = document.querySelector("#display-num");
+
+digitButtons.forEach((digitBtn) => {
+  digitBtn.addEventListener("click", () => {
+    const displayedNum = displayNum.textContent;
+    const newDigit = digitBtn.textContent;
+
+    if (displayedNum === "0") {
+      displayNum.textContent = newDigit;
+    } else {
+      displayNum.textContent += digitBtn.textContent;
+    }
+  });
+});

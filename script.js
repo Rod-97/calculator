9d6fd8d3786fb7c6582ad9013f7e4e329fc2c818
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-03-13 20:32:49 +0100

    populate display

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ function divide(a, b) {
 let num1 = 0;
 let operator = null;
 let num2 = 0;
+
+function operate(num1, operator, num2) {
+  if (operator === "+") return add(num1, num2);
+  if (operator === "-") return subtract(num1, num2);
+  if (operator === "x") return multiply(num1, num2);
+  if (operator === "/") return divide(num1, num2);
+}
+
+const display = document.querySelector("#display-num");
+
+function populateDisplay(newDigit) {
+  if (display.textContent === "0") {
+    display.textContent = newDigit;
+  } else {
+    display.textContent += newDigit;
+  }
+}
+
+const digitBtns = document.querySelectorAll(".digit-btn");
+
+digitBtns.forEach((digitBtn) => {
+  digitBtn.addEventListener("click", () =>
+    populateDisplay(digitBtn.textContent)
+  );
+});

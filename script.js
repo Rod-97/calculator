8a2a47b0550855db13785a918c2de0cd242ea0b8
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-03-15 16:42:16 +0100

    basic functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 / num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(num1, operator, num2) {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  if (operator === "+") return add(num1, num2);
+  if (operator === "-") return subtract(num1, num2);
+  if (operator === "x") return multiply(num1, num2);
+  if (operator === "/") return divide(num1, num2);
+}

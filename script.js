9f13101e4d11542ebcc9d936c71f024ee09f090d
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-03-15 17:28:13 +0100

    operator and equal btns

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const display = document.querySelector("#display-num");
 const digitBtns = document.querySelectorAll(".digit-btn");
+const operatorBtns = document.querySelectorAll(".operator-btn");
+const equalBtn = document.querySelectorAll(".equal-btn")[0];
+
+let operation = [];
 
 function add(num1, num2) {
   return num1 + num2;
 }
 
 function subtract(num1, num2) {
-  return num1 / num2;
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
@@ -31,9 +35,54 @@ function populateDisplay(newNum) {
   else display.textContent += newNum;
 }
 
+function clearDisplay() {
+  display.textContent = "0";
+}
+
+function displayResult(result) {
+  clearDisplay();
+  populateDisplay(result);
+}
+
 digitBtns.forEach((digitBtn) => {
   digitBtn.addEventListener("click", () => {
     const digit = digitBtn.textContent;
+
+    if (operation.length === 0) {
+      operation.push(digit);
+    } else if (operation.length === 1) {
+      operation[0] += digit;
+    } else if (operation.length === 2) {
+      clearDisplay();
+      operation.push(digit);
+    } else if (operation.length === 3) {
+      operation[2] += digit;
+    }
+
     populateDisplay(digit);
   });
+});
+
+operatorBtns.forEach((operatorBtn) => {
+  operatorBtn.addEventListener("click", () => {
+    const operator = operatorBtn.textContent;
+    if (operation.length === 0) {
+      return;
+    } else if (operation.length === 1) {
+      operation.push(operator);
+    } else if (operation.length === 2) {
+      operation[1] = operator;
+    } else if (operation.length === 3) {
+      const result = operate(...operation);
+      operation = [result, operator];
+      displayResult(result);
+    }
+  });
+});
+
+equalBtn.addEventListener("click", () => {
+  if (operation.length !== 3) return;
+  const result = operate(...operation);
+  operation = [result];
+  displayResult(result);
 });
